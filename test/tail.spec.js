4b95bc4f1686e9af951d051eb818680d1e5c104b
Alexey Ershov <alex4rom@gmail.com>
2015-04-02 18:27:44 +0300

    Modify 'tail' spec

--- test/tail.spec.js
+++ test/tail.spec.js
@@ -19,12 +19,16 @@ describe('cmd.tail', function () {
         expect(cmd.tail(2).with(1, 2, 3)).to.deep.equal([2, 3]);
     });
 
-    it('returns last two values when negative argument is given', function () {
+    it('returns last two values when negative argument was given', function () {
         expect(cmd.tail(-2).with(1, 2, 3)).to.deep.equal([2, 3]);
     });
 
     it('returns all the values if number is bigger than amount of arguments', function () {
         expect(cmd.tail(100).with(1, 2, 3)).to.deep.equal([1, 2, 3]);
+    });
+
+    it('returns all the values if no argument was given', function () {
+        expect(cmd.tail().with(1, 2, 3)).to.deep.equal([1, 2, 3]);
     });
 
     it('ignores extra arguments', function () {
